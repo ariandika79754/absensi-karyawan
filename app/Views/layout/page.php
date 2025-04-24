f4350a204bc48dfc87f83a56bcf2409f7e1e99bb
Ari Andika <ariandika79754@gmail.com>
2025-04-24 16:23:15 +0700

    update by ari

--- app/Views/layout/page.php
+++ app/Views/layout/page.php
@@ -146,7 +146,7 @@
                                         <div class="dropdown-divider"></div>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="/<?= strtolower(session()->get('role')); ?>/profile">
+                                        <a class="dropdown-item" href="/<?= strtolower(session()->get('role')); ?>/profil">
                                             <i class="bx bx-user me-2"></i>
                                             <span class="align-middle">My Profile</span>
                                         </a>
